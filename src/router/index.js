ffc12b5e83104a2339ecbde9c24d8b8f43014df0
mdabhn <mdabhn@gmail.com>
2020-07-02 18:46:52 +0600

    Bug fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,19 @@ import VueRouter from 'vue-router'
 
 import '../assets/tailwind.css'
 
-import Home from '../components/Home'
+// import Home from '../components/Home'
 import Inventory from '../components/Inventory'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  // },
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/inventory',
     name: 'Inventory',
     component: Inventory,
   },
